409493169fb997d96ad380e088689b712e7a7d62
Isidro Lorenzo <isidrolorenzo_lp@hotmail.com>
2017-06-01 17:55:00 -0300

    Delete mostrar_favores.php

--- mostrar_favores.php
+++
@@ -1,7 +0,0 @@
-<?php
-include_once ('../db/connect.php');
-$link = conectar();
-$result = mysqli_query($link, "SELECT * FROM favor WHERE (CURDATE() <= fechalimite) AND (activo = 1)");
-mysqli_close($link);
-include_once("print_favores.php");
-?>
