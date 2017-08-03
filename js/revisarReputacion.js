b9714683f77c3e4d914b51d9b83e3f8cd9f19f2d
Federico <Fede_FJM@hotmail.com>
2017-08-03 02:23:05 -0300

    Se corrigieron errores en las validaciones de los rangos de reputacion.

--- js/revisarReputacion.js
+++ js/revisarReputacion.js
@@ -1,5 +1,6 @@
 $(function(){
 	$("#agregarReputacion").submit(function(event){
+		if (validarCamposAgregar(event)){
 		var nombre = $("#nombrereputacion").val();
 		var puntajemin = $("#puntajemin").val();
 		var puntajemax = $("#puntajemax").val();
@@ -19,26 +20,24 @@ $(function(){
 				alert ("El rango de puntajes superpone a otro o ya existe para otra reputacion.");
 				event.preventDefault();
 			}
-	});
+	}});
 	$("#modificarReputacion").submit(function(event){
+	if (validarCamposModificar(event)){
 	var nombre = $("#nombrereputacion3").val();
 	var puntajemin = $("#puntajemin2").val();
 	var puntajemax = $("#puntajemax2").val();
+	var id = $("#idreputacion").val();
 	$.ajax({
-				data: {"nombre":nombre, "puntajemin":puntajemin, "puntajemax":puntajemax},
+				data: {"nombre":nombre, "puntajemin":puntajemin, "puntajemax":puntajemax, "id":id},
 				url: './revisarreputaciones.php',
 				type: 'post',
 				async: false,
 		}).done(function (respuesta){
 			res = respuesta;
 		});
-		if (res === "1"){
-			alert ("El nombre de la reputacion ya existe.");
-			event.preventDefault();
-		}
 		if (res === "2"){
 			alert ("El rango de puntajes superpone a otro, o ya existe para otra reputacion.");
 			event.preventDefault();
 		}
-	});
+	}});
 });
